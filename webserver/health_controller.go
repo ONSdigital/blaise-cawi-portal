56001d76f94e7fffd5701e1b481e744876c2c6ce
Nik Webb <nik.webb@ons.gov.uk>
2021-07-19 17:21:34 +0100

    Add _ah endpoints

--- webserver/health_controller.go
+++ webserver/health_controller.go
@@ -17,6 +17,10 @@ type HealthController struct {
 func (healthController *HealthController) AddRoutes(httpRouter *gin.Engine) {
 	httpRouter.GET("/health", healthController.HealthEndpoint)
 	httpRouter.GET("/cawi-portal/:version/health", healthController.HealthEndpoint)
+	httpRouter.GET("/_ah/*command", func(context *gin.Context) {
+		command := context.Param("command")
+		context.JSON(http.StatusOK, command)
+	})
 }
 
 func (healthController *HealthController) HealthEndpoint(context *gin.Context) {
