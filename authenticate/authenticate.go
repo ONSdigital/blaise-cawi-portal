c92e39dd2d3f929bc3b7380311ce2d76a32ad297
Sam Bryant <sam.bryant@armakuni.com>
2021-07-26 14:15:38 +0100

    Add postcode attempts to login

--- authenticate/authenticate.go
+++ authenticate/authenticate.go
@@ -14,13 +14,17 @@ import (
 )
 
 const (
-	JWT_TOKEN_KEY           = "jwt_token"
-	NO_ACCESS_CODE_ERR      = "Enter an access code"
-	INVALID_LENGTH_ERR      = "Enter a 12-character access code"
-	NOT_RECOGNISED_ERR      = "Access code not recognised. Enter the code again"
-	INTERNAL_SERVER_ERR     = "We were unable to process your request, please try again"
-	POSTCODE_VALIDATION_ERR = "Postcode not recognised, please try again"
-	ISSUER                  = "social-surveys-web-portal"
+	JWT_TOKEN_KEY             = "jwt_token"
+	NO_ACCESS_CODE_ERR        = "Enter an access code"
+	INVALID_LENGTH_ERR        = "Enter a 12-character access code"
+	NOT_RECOGNISED_ERR        = "Access code not recognised. Enter the code again"
+	INTERNAL_SERVER_ERR       = "We were unable to process your request, please try again"
+	POSTCODE_VALIDATION_ERR   = "Postcode not recognised, please try again"
+	POSTCODE_ATTEMPT_ERR      = "This uac is currently locked due to repeated failed postcode attempts"
+	ISSUER                    = "social-surveys-web-portal"
+	MAX_POSTCODE_ATTEMPTS     = 5
+	POSTCODE_ATTEMPT_TIMEOUT  = time.Duration(30 * time.Minute)
+	POSTCODE_TIMESTAMP_FORMAT = "2006-01-02 15:04:05.999999999 -0700 MST"
 )
 
 var expirationTime = "2h"
@@ -118,6 +122,19 @@ func (auth *Auth) Login(context *gin.Context, session sessions.Session) {
 		return
 	}
 
+	if uacInfo.PostcodeAttempts >= MAX_POSTCODE_ATTEMPTS {
+		postcodeAttemptTimestamp, err := time.Parse(POSTCODE_TIMESTAMP_FORMAT, uacInfo.PostcodeAttemptTimestamp)
+		if err != nil {
+			log.Println(err)
+			NotAuthWithError(context, INTERNAL_SERVER_ERR)
+			return
+		}
+		if time.Now().UTC().Before(postcodeAttemptTimestamp.Add(POSTCODE_ATTEMPT_TIMEOUT)) {
+			NotAuthWithError(context, POSTCODE_ATTEMPT_ERR)
+			return
+		}
+	}
+
 	signedToken, err := auth.JWTCrypto.EncryptJWT(uac, &uacInfo)
 	if err != nil {
 		log.Println(err)
@@ -152,31 +169,76 @@ func (auth *Auth) LoginPostcode(context *gin.Context, session sessions.Session) 
 		return
 	}
 
+	uacInfo, err := auth.BusApi.GetUacInfo(claim.UAC)
+	if err != nil {
+		log.Println(err)
+		NotAuthPostcodeWithError(context, INTERNAL_SERVER_ERR)
+		return
+	}
+
+	if uacInfo.PostcodeAttempts > 0 {
+		postcodeAttemptTimestamp, err := time.Parse(POSTCODE_TIMESTAMP_FORMAT, uacInfo.PostcodeAttemptTimestamp)
+		if err != nil {
+			log.Println(err)
+			NotAuthPostcodeWithError(context, INTERNAL_SERVER_ERR)
+			return
+		}
+		if time.Now().UTC().After(postcodeAttemptTimestamp.Add(POSTCODE_ATTEMPT_TIMEOUT)) {
+			uacInfo, err = auth.BusApi.ResetPostcodeAttempts(claim.UAC)
+			if err != nil {
+				log.Println(err)
+				NotAuthPostcodeWithError(context, INTERNAL_SERVER_ERR)
+				return
+			}
+		}
+	}
+
+	if uacInfo.PostcodeAttempts >= MAX_POSTCODE_ATTEMPTS {
+		NotAuthWithError(context, POSTCODE_ATTEMPT_ERR)
+		return
+	}
+
 	casePostcode, err := auth.BlaiseRestApi.GetPostCode(claim.UacInfo.InstrumentName, claim.UacInfo.CaseID)
 	if err != nil {
-		fmt.Println(err)
-		NotAuthWithError(context, INTERNAL_SERVER_ERR)
+		log.Println(err)
+		NotAuthPostcodeWithError(context, INTERNAL_SERVER_ERR)
 		return
 	}
 	if !auth.ValidatePostcode(enteredPostcode, casePostcode) {
-		fmt.Println(enteredPostcode)
-		fmt.Println(casePostcode)
-		NotAuthWithError(context, POSTCODE_VALIDATION_ERR)
+		uacInfo, err := auth.BusApi.IncrementPostcodeAttempts(claim.UAC)
+		if err != nil {
+			log.Println(err)
+			NotAuthPostcodeWithError(context, INTERNAL_SERVER_ERR)
+			return
+		}
+		if uacInfo.PostcodeAttempts >= MAX_POSTCODE_ATTEMPTS {
+			NotAuthWithError(context, POSTCODE_ATTEMPT_ERR)
+			return
+		}
+		NotAuthPostcodeWithError(context, POSTCODE_VALIDATION_ERR)
 		return
 	}
 
 	signedToken, err := auth.JWTCrypto.EncryptValidatedPostcodeJWT(claim)
 	if err != nil {
 		log.Println(err)
-		NotAuthWithError(context, INTERNAL_SERVER_ERR)
+		NotAuthPostcodeWithError(context, INTERNAL_SERVER_ERR)
 		return
 	}
 
 	session.Set(JWT_TOKEN_KEY, signedToken)
 	if err := session.Save(); err != nil {
 		log.Println(err)
-		NotAuthWithError(context, INTERNAL_SERVER_ERR)
+		NotAuthPostcodeWithError(context, INTERNAL_SERVER_ERR)
 		return
+	}
+	if uacInfo.PostcodeAttempts > 0 {
+		_, err = auth.BusApi.ResetPostcodeAttempts(claim.UAC)
+		if err != nil {
+			log.Println(err)
+			NotAuthPostcodeWithError(context, INTERNAL_SERVER_ERR)
+			return
+		}
 	}
 	context.Redirect(http.StatusFound, fmt.Sprintf("/%s/", claim.UacInfo.InstrumentName))
 	context.Abort()
@@ -207,6 +269,11 @@ func notAuth(context *gin.Context) {
 
 func NotAuthWithError(context *gin.Context, errorMessage string) {
 	context.HTML(http.StatusUnauthorized, "login.tmpl", gin.H{"error": errorMessage})
+	context.Abort()
+}
+
+func NotAuthPostcodeWithError(context *gin.Context, errorMessage string) {
+	context.HTML(http.StatusUnauthorized, "postcode.tmpl", gin.H{"error": errorMessage})
 	context.Abort()
 }
 
