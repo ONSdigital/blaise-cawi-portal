aea5cf1c0f48e2bb6f10046aea6cee1a6c4a3b4f
James Anthony Williams <81305008+JAWilliamsONS@users.noreply.github.com>
2024-03-18 12:03:18 +0000

    feat: add 'disabled' property in datastore to CAWI login

--- busapi/uac_info.go
+++ busapi/uac_info.go
@@ -3,9 +3,11 @@ package busapi
 type UacInfo struct {
 	InstrumentName string `json:"instrument_name"`
 	CaseID         string `json:"case_id"`
+	Disabled       bool   `json:"disabled"`
 }
 
 func (uacInfo *UacInfo) InvalidCase() bool {
 	return uacInfo.InstrumentName == "" || uacInfo.CaseID == "" ||
-		uacInfo.InstrumentName == "unknown" || uacInfo.CaseID == "unknown"
+		uacInfo.InstrumentName == "unknown" || uacInfo.CaseID == "unknown" ||
+		uacInfo.Disabled
 }
