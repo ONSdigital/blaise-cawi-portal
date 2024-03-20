2a04afa8e4dc3c58549a8b3544c963e2dcb65315
James Anthony Williams <81305008+JAWilliamsONS@users.noreply.github.com>
2024-03-20 11:13:43 +0000

    wip: add json tag for disabled field

--- busapi/uac_info.go
+++ busapi/uac_info.go
@@ -3,10 +3,10 @@ package busapi
 type UacInfo struct {
 	InstrumentName string `json:"instrument_name"`
 	CaseID         string `json:"case_id"`
-	Disabled       bool
+	Disabled       bool   `json:"disabled"`
 }
 
 func (uacInfo *UacInfo) InvalidCase() bool {
 	return uacInfo.InstrumentName == "" || uacInfo.CaseID == "" ||
-		uacInfo.InstrumentName == "unknown" || uacInfo.CaseID == "unknown"
+		uacInfo.InstrumentName == "unknown" || uacInfo.CaseID == "unknown" || uacInfo.Disabled
 }
